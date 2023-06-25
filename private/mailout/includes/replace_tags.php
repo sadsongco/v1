b9fb6c31c01a37b08887c3dfa3440b55e8a7e896
Nigel Powell <nigel@thesadsongco.com>
2023-06-25 10:09:31 -0500

    Remove double protocol

--- private/mailout/includes/replace_tags.php
+++ private/mailout/includes/replace_tags.php
@@ -5,8 +5,8 @@ function replace_tags($body_template, $row) {
     $row['secure_id'] = $secure_id;
     $protocol = 'http';
     if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') $protocol .= 's';
-    if (!isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] == '') $_SERVER['HTTP_HOST'] = "https://unbelievabletruth.co.uk";
-    $row['host'] = "$protocol://".$_SERVER['HTTP_HOST'];
+    if (!isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] == '') $row['host'] = "https://unbelievabletruth.co.uk";
+    else $row['host'] = "$protocol://".$_SERVER['HTTP_HOST'];
     foreach ($row as $tag_name=>$tag_content) {
         if ($tag_name == 'name' && $tag_content == '') $tag_content = 'Music Friend';
         $body_template = str_replace("<!--{{".$tag_name."}}-->", $tag_content, $body_template);
