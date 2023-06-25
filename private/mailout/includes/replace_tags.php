17065971c3411675865e9a292f14999a7e1624d8
Nigel Powell <nigel@thesadsongco.com>
2023-06-25 10:04:42 -0500

    Cron host issue resolved?

--- private/mailout/includes/replace_tags.php
+++ private/mailout/includes/replace_tags.php
@@ -5,6 +5,7 @@ function replace_tags($body_template, $row) {
     $row['secure_id'] = $secure_id;
     $protocol = 'http';
     if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') $protocol .= 's';
+    if (!isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] == '') $_SERVER['HTTP_HOST'] = "https://unbelievabletruth.co.uk";
     $row['host'] = "$protocol://".$_SERVER['HTTP_HOST'];
     foreach ($row as $tag_name=>$tag_content) {
         if ($tag_name == 'name' && $tag_content == '') $tag_content = 'Music Friend';
