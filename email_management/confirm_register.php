05ecbec63d45c909d9ccf2fe42043a745b09bd55
Nigel Powell <nigel@thesadsongco.com>
2023-09-29 18:06:46 +0100

    confirm_register path error corrected

--- email_management/confirm_register.php
+++ email_management/confirm_register.php
@@ -8,8 +8,8 @@ require '../lib/mustache.php-main/src/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
 $m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'../php/templates'),
-    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'../php/templates/partials')
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../php/templates'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../php/templates/partials')
 ));
 
 // auth
