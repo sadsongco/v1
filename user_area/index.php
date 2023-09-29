282a6e1eb4f76885af2065c84aacd3a070688123
Nigel Powell <nigel@thesadsongco.com>
2023-09-29 18:00:54 +0100

    Mustache template paths

--- user_area/index.php
+++ user_area/index.php
@@ -17,8 +17,8 @@ require '../lib/mustache.php-main/src/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
 $m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader('templates'),
-    'partials_loader' => new Mustache_Loader_FilesystemLoader('templates/partials')
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates/partials')
 ));
 
 $protocol = 'http';
