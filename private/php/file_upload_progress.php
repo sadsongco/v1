25c985941aee5ac0da3c2f85d9ae012884c99544
Nigel Powell <nigel@thesadsongco.com>
2023-10-16 16:05:26 +0100

    Session uploading

--- private/php/file_upload_progress.php
+++ private/php/file_upload_progress.php
@@ -2,23 +2,6 @@
 
 include (__DIR__."/../../php/includes/p_2.php");
 
-$filename = "progress.txt";
-
-if (!file_exists($filename)) $fp = fopen($filename, "w+");
-else $fp = fopen($filename, "r+");
-
-if (filesize($filename) == 0 && !isset($_SESSION)) {
-    fclose($fp);
-    exit("NO PROGRESS");
-}
-
-if (filesize($filename) == 0 && isset($_SESSION)) {
-    fwrite($fp, p_2($_SESSION));
-}
-
-$output = fread($fp, filesize($filenname));
-fclose($fp);
-
-echo $output;
+if (isset($_SESSION)) echo "UPLOADING FILE...";
 
 ?>
