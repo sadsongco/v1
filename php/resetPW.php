82e25dc5a8c3c5e91704179bb028fdb4ab0300c4
Nigel Powell <nigel@thesadsongco.com>
2023-10-20 11:06:41 +0100

    Template error

--- php/resetPW.php
+++ php/resetPW.php
@@ -20,12 +20,14 @@ require '../lib/mustache.php-main/src/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
 $m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates'),
-    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates/partials')
+    'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/templates'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/templates/partials')
 ));
 
 $host = getHost();
 
-echo $m->render("requestPWreset", ["base_dir"=>$host]);
+echo __DIR__.'/templates';
+
+// echo $m->render("requestPWreset", ["base_dir"=>$host]);
 
 ?>
