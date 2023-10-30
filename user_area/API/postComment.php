53add4ada7b2646f19a0dca4f567868e98545eb1
Nigel Powell <nigel@thesadsongco.com>
2023-10-30 15:05:37 +0000

    Comments, specific spinners

--- user_area/API/postComment.php
+++ user_area/API/postComment.php
@@ -4,7 +4,9 @@ require_once("../../../secure/scripts/ut_a_connect.php");
 
 define("RELATIVE_ROOT", "/../../../");
 
+// utilities
 include(__DIR__."/../../php/includes/p_2.php");
+include_once(__DIR__."/includes/getHost.php");
 
 // auth
 require __DIR__ . '/../../php/vendor/autoload.php';
@@ -24,6 +26,15 @@ $m = new Mustache_Engine(array(
     'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../templates/partials')
 ));
 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../../private/mailout/API/vendor/autoload.php';
+
 function getCommentNotify($db, $reply) {
     try {
         $query = "SELECT notify, user_id FROM comments WHERE comment_id = ?;";
@@ -37,7 +48,7 @@ function getCommentNotify($db, $reply) {
     }
 }
 
-function sendNotification($db, $user_id) {
+function sendNotification($db, $m, $user_id, $article_id) {
     try {
         $query = "SELECT email FROM users WHERE id = ?;";
         $stmt = $db->prepare($query);
@@ -48,19 +59,46 @@ function sendNotification($db, $user_id) {
         error_log($e->getMessage());
         return;
     }
-    echo "GOING TO SEND AN EMAIL TO $email";
+
+    require_once("../../../secure/mailauth/ut.php");
+    $host = getHost();
+    $email_html = $m->render('replyNotificationEmailHTML', ["host"=>$host, "article_id"=>$article_id]);
+    $email_txt = $m->render('replyNotificationEmailTxt', ["host"=>$host, "article_id"=>$article_id]);
+    $subject = "Unbelievable Truth - there's a reply to your comment";
+
+    // set up PHP Mailer
+    //Passing `true` enables PHPMailer exceptions
+    $mail = new PHPMailer(true);
+
+    // setup email variables
+    $mail->isSMTP();
+    $mail->Host = $mail_auth['host'];
+    $mail->SMTPAuth = true;
+    $mail->SMTPKeepAlive = false; //SMTP connection will not close after each email sent, reduces SMTP overhead
+    $mail->Port = 25;
+    $mail->Username = $mail_auth['username'];
+    $mail->Password = $mail_auth['password'];
+    $mail->setFrom($mail_auth['from']['address'], "Unbelievable Truth - website");
+    $mail->addReplyTo($mail_auth['reply']['address'], "Unbelievable Truth - website");
+    $mail->addAddress($email);
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $email_html;
+    $mail->AltBody = $email_txt;
+
+    $mail->send();
 }
 
 $reply = null;
 $notify = 0;
-p_2($_POST);
 
 if (isset($_POST['notify'])) $notify = true;
 
 if (isset($_POST['comment_reply_id']) && intval($_POST['comment_reply_id']) != 0) {
     $reply = $_POST['comment_reply_id'];
-    $emailNotification = getCommentNotify($db, $reply);
-    if ($notify['notify'] == 1) sendNotification($db, $notify['user_id']);
+    $email_notification = getCommentNotify($db, $reply);
+    if ($email_notification['notify'] == 1) sendNotification($db, $m, $email_notification['user_id'], $_POST['article_id']);
 }
 
 
