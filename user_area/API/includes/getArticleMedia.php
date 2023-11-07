31aaf7e604e60729f196de25725a4520dcab6ea8
Nigel Powell <nigel@thesadsongco.com>
2023-11-07 16:12:14 +0000

    Image alignment init

--- user_area/API/includes/getArticleMedia.php
+++ user_area/API/includes/getArticleMedia.php
@@ -4,25 +4,22 @@ function makeUniqueToken($auth, $track) {
     return hash('sha1', $auth->getUsername().$track["filename"]);
 }
 
-function getMediaArr($table, $ids, $db) {
+function getMediaArr($table, $id, $db) {
     $id_id = $table == "media" ? "media_id" : "image_id";
     $query = "SELECT $id_id, filename, title, notes FROM $table WHERE $id_id = ?";
-    for ($x = 1; $x < sizeof($ids); $x++) {
-        $query .= " OR $id_id = ?";
-    }
-    $query .= ";";
     $stmt = $db->prepare($query);
-    $stmt->execute($ids);
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->execute([$id]);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result[0];
 }
 
 function getMedia($content, $db, $auth, $m, $host) {
     // Get audio
     preg_match_all('/{{a::([0-9])+}}/', $content, $audio_ids);
     if (sizeof($audio_ids[1]) > 0) {
-        $audio_arr = getMediaArr("media", $audio_ids[1], $db);
-        foreach ($audio_arr AS $track) {
-            $replace_str = "{{a::".$track["media_id"]."}}";
+        foreach ($audio_ids[1] as $key=>$audio_id) {
+            $track = getMediaArr("media", $audio_id, $db);
+            $replace_str = $audio_ids[0][$key];
             $track["token"] = makeUniqueToken($auth, $track);
             try {
                 $query = "INSERT IGNORE INTO streaming_tokens VALUES (0, ?, ?, ?);";
@@ -40,15 +37,27 @@ function getMedia($content, $db, $auth, $m, $host) {
         }
     }
     // get images
-    preg_match_all('/{{i::([0-9])+}}/', $content, $image_ids);
+    preg_match_all('/{{i::([0-9])+(?:::)?(l|r)?.?}}/', $content, $image_ids);
     if (sizeof($image_ids[1]) > 0) {
-        $image_arr = getMediaArr("images", $image_ids[1], $db);
-        foreach ($image_arr as $image) {
-            $replace_str = "{{i::".$image["image_id"]."}}";
+        foreach ($image_ids[1] as $key=>$image_id) {
+            $image = getMediaArr("images", $image_id, $db);
             $image["path"] = IMAGE_UPLOAD_PATH.$image["filename"];
-            $image["size_string"] = getimagesize(__DIR__.RELATIVE_ROOT.$image["path"])[3];
-            $replace_el = $m->render("imageTag", $image);
-            $content = preg_replace("/$replace_str/", $replace_el, $content);
+            $image_metadata = getimagesize(__DIR__.RELATIVE_ROOT.$image["path"]);
+            $image["size_string"] = $image_metadata[3];
+            $image["aspect_ratio"] = $image_metadata[0] . "/".$image_metadata[1];
+            $replace_str = $image_ids[0][$key];
+            if (isset($image_ids[2][$key])) {
+                switch ($image_ids[2][$key]) {
+                    case "l":
+                        $image["float"] = "left";
+                        break;
+                    case "r":
+                        $image["float"] = "right";
+                        break;
+                }
+                $replace_el = $m->render("imageTag", $image);
+                $content = preg_replace("/$replace_str/", $replace_el, $content);
+            }
         }
     }
     return nl2br($content);
