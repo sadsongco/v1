f30253418fb602709fa8d48eee1af890e4bc764a
Nigel Powell <nigel@thesadsongco.com>
2023-11-16 14:11:07 +0000

    troubleshoot admin enum

--- private/php/includes/privateIncludes.php
+++ private/php/includes/privateIncludes.php
@@ -29,6 +29,7 @@ function getPosters($db) {
         $query = "SELECT column_type FROM information_schema.columns WHERE table_name = 'articles' AND column_name = 'posted_by'";
         $result = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
         $result_str = str_replace(array("enum('", "')", "''"), array('', '', "'"), $result[0]["COLUMN_TYPE"]);
+        echo $result_str;
         $arr = explode("','", $result_str);
         if (sizeof($arr) == 0) $arr = ["Nigel", "Andy", "Jason", "Admin"];
         $posters = [];
