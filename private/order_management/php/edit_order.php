7b49110c637bb6a3504e8081620dd84a17736da7
Nigel Powell <nigel@thesadsongco.com>
2025-03-02 08:02:32 +0000

    Edit order functionality

--- private/order_management/php/edit_order.php
+++ private/order_management/php/edit_order.php
@@ -2,4 +2,49 @@
 
 require_once(__DIR__."/includes/order_includes.php");
 
-header ('HX-Trigger:editOrder');
+extract($_GET);
+
+try {
+    $query = "SELECT
+    order_id,
+    shipping_method,
+    subtotal,
+    shipping,
+    vat,
+    total,
+    Customers.customer_id,
+    name,
+    address_1,
+    address_2,
+    city,
+    postcode,
+    country
+    FROM Orders
+    JOIN Customers ON Orders.customer_id = Customers.customer_id
+    WHERE order_id = ?";
+    $stmt = $db->prepare($query);
+    $stmt->execute([$order_id]);
+    $order = $stmt->fetch(PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    header ('HX-Trigger:editOrder');
+    echo "Couldn't get order details from database: " .$e->getMessage();
+    exit();
+}
+
+$params = [];
+
+foreach ($order as $key => $value) {
+    $type = "text";
+    switch (gettype($value)) {
+        case "string":
+            $type = "text";
+            break;
+        case "int":
+            $type = "number";
+            break;
+    }
+    $label = ucwords(str_replace("_", " ", $key));
+    $params[] = ["key"=>$key, "value"=>$value, "type"=>$type, "label"=>$label];
+}
+// header ('HX-Trigger:editOrder');
+echo $m->render("editOrder", ["orders"=>$params]);
---
+++ private/order_management/php/update_order.php
@@ -0,0 +1,59 @@
+<?php
+require_once(__DIR__."/includes/order_includes.php");
+
+extract($_POST);
+
+if ($submit == "cancel") {
+    exit("<script>
+        document.getElementById('order_$order_id').scrollIntoView({behaviour: 'smooth', block: 'start'});
+    </script>");
+}
+
+try {
+    $query = "UPDATE Orders
+    SET
+    shipping_method = ?,
+    subtotal = ?,
+    shipping = ?,
+    vat = ?,
+    total = ?
+    WHERE order_id = ?";
+    $params = [
+        $shipping_method,
+        $subtotal,
+        $shipping,
+        $vat,
+        $total,
+        $order_id
+    ];
+    $stmt = $db->prepare($query);
+    $stmt->execute($params);
+
+    $query = "UPDATE Customers
+    SET
+    name = ?,
+    address_1 = ?,
+    address_2 = ?,
+    city = ?,
+    postcode = ?,
+    country = ?
+    WHERE customer_id = ?";
+    $params = [
+        $name,
+        $address_1,
+        $address_2,
+        $city,
+        $postcode,
+        $country,
+        $customer_id
+    ];
+    $stmt = $db->prepare($query);
+    $stmt->execute($params);
+} catch(Exception $e) {
+    echo "couldn't update order: ".$e->getMessage();
+    exit();
+}
+
+header ('HX-Trigger:updateOrderList');
+
+echo "<p>Order $order_id Updated</p>";
